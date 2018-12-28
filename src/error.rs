312bdee1c587a9622d78cf555a0fa199c00ee3e6
Ryan Leckey <ryan@launchbadge.com>
2018-12-28 10:03:45 -0800

    Add optional pass-through failure integration

--- src/error.rs
+++ src/error.rs
@@ -24,3 +24,33 @@ impl<E> StdError for Error<E> where E: 'static + Debug + Send + Sync {}
 
 #[cfg(feature = "actix-web")]
 impl<E> actix_web::ResponseError for Error<E> where E: 'static + Debug + Send + Sync {}
+
+#[cfg(feature = "failure")]
+impl Error<failure::Error> {
+    /// Attempts to downcast this Error to a particular Fail type.
+    #[inline]
+    pub fn downcast<T: failure::Fail>(self) -> Result<T, Self> {
+        match self {
+            Error::Execute(err) => err.downcast().map_err(Error::Execute),
+            err => Err(err),
+        }
+    }
+
+    /// Attempts to downcast this Error to a particular Fail type by reference.
+    #[inline]
+    pub fn downcast_ref<T: failure::Fail>(&self) -> Option<&T> {
+        match self {
+            Error::Execute(err) => err.downcast_ref(),
+            _ => None,
+        }
+    }
+
+    /// Attempts to downcast this Error to a particular Fail type by mutable reference.
+    #[inline]
+    pub fn downcast_mut<T: failure::Fail>(&mut self) -> Option<&mut T> {
+        match self {
+            Error::Execute(err) => err.downcast_mut(),
+            _ => None,
+        }
+    }
+}
