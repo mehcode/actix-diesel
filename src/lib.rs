222e8182c84da291a8e362d27da771ae500277df
Ryan Leckey <ryan@launchbadge.com>
2019-02-13 18:29:51 -0800

    Still export Error for compat

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ pub mod dsl;
 mod error;
 mod executor;
 
-pub use self::{builder::Builder, db::Database, error::AsyncError};
+pub use self::{builder::Builder, db::Database, error::AsyncError, error::AsyncError as Error};
